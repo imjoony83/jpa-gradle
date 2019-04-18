e629fa50ebd26b64b03374a3e26393b07c657988
seongjoon@malangstudio.com <seongjoon@malangstudio.com>
2019-04-19 01:07:59 +0900

    초기 데모 데이터 추가.

--- src/main/java/com/github/imjoony83/jpagradle/listener/InitDemoDataListener.java
+++ src/main/java/com/github/imjoony83/jpagradle/listener/InitDemoDataListener.java
@@ -1,21 +1,33 @@
 package com.github.imjoony83.jpagradle.listener;
 
+import com.github.imjoony83.jpagradle.constant.CouponStatus;
+import com.github.imjoony83.jpagradle.constant.GradeType;
+import com.github.imjoony83.jpagradle.entity.Coupon;
 import com.github.imjoony83.jpagradle.entity.Customer;
+import com.github.imjoony83.jpagradle.entity.CustomerCoupon;
 import com.github.imjoony83.jpagradle.repository.CouponJpaRepository;
 import com.github.imjoony83.jpagradle.repository.CustomerCouponJpaRepository;
 import com.github.imjoony83.jpagradle.repository.CustomerJpaRepository;
-import com.github.javafaker.Faker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class InitDemoDataListener implements ApplicationListener<ApplicationReadyEvent> {
+
+    final Logger logger = LoggerFactory.getLogger(InitDemoDataListener.class);
 
     private final CouponJpaRepository couponJpaRepository;
     private final CustomerJpaRepository customerJpaRepository;
@@ -23,23 +35,31 @@ public class InitDemoDataListener implements ApplicationListener<ApplicationRead
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
-        // TODO Auto-generated method stub
 
-        // add Customer
-        Faker faker = new Faker();
-        // faker.name();
-
-        Arrays.asList(faker.name(), faker.name(), faker.name()).stream().forEach(name -> {
-//            Customer customer - Customer.
-//            Customer customer = Customer.builder().name(name).email(name + "@mail.com").grade(0).build();
-//            customerJpaRepository.save(customer);
+        // add Customers
+        Stream.of("customer_1", "customer_2", "customer_3").forEach(name -> {
+            Customer customer = Customer.builder().name(name).email(name + "@mail.com").gradeType(GradeType.LEVEL_1).build();
+            customerJpaRepository.save(customer);
         });
 
-        // add Coupon
+        // add Coupons
+        Stream.of("coupon_1", "coupon_2").forEach(couponName-> {
+            Coupon coupon = Coupon.builder().name(couponName).expireDate(LocalDate.now().plusDays(1)).status(CouponStatus.VALID).build();
+            couponJpaRepository.save(coupon);
+        });
 
-        // add CustomerCoupon
+        // add CustomerCoupons
+        Collection<Customer> customers = customerJpaRepository.findAll();
+        Collection<Coupon> coupons = couponJpaRepository.findAll();
 
+        final BiConsumer<Collection<Customer>, Coupon> makeCustomerCoupons = (customersCollection, coupon) -> {
+            customersCollection.forEach(customer -> {
+                CustomerCoupon customerCoupon = CustomerCoupon.builder().coupon(coupon).customer(customer).status(CouponStatus.NOTUSE).build();
+                customerCouponJpaRepository.save(customerCoupon);
+            });
+        };
 
+        coupons.forEach(coupon-> makeCustomerCoupons.accept(customers, coupon) );
     }
 
 }
